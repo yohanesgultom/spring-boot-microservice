144a9375743ebd1221c7b83a12b4c89c7fd203e4
Yohanes Gultom <yohanes.gultom@gmail.com>
2021-03-03 17:53:26 +0700

    updated docker-compose.yml

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose run service /bin/sh -c "./wait-for-it.sh mssql:1433 -s -t 90 -- mvn clean test -Dtests=integration"; docker-compose down -v;
+docker-compose run -u root service /bin/sh -c "./wait-for-it.sh mssql:1433 -s -t 90 -- mvn clean test -Dtests=integration"; docker-compose down -v;
